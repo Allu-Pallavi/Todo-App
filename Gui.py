ed7f0d7c54d981207976265db4351fad00dcf146
Pallavi-Allu <pallavi29.allu@gmail.com.com>
2025-07-21 11:34:37 +0530

    add complete and exit

--- Gui.py
+++ Gui.py
@@ -1,31 +1,66 @@
 import Functions
-import FreeSimpleGUI as fsg
+import FreeSimpleGUI  as fsg  # ✅ Corrected name
 
-lable =fsg.Text("Type in  Todo ")
-input_box = fsg.InputText(tooltip =" ADD A TODO",key='todo')
-add_button =fsg.Button("Add")
+label = fsg.Text("Type in a Todo")
+input_box = fsg.InputText(tooltip="Add a todo", key='todo')
+add_button = fsg.Button("Add")
+complete_button = fsg.Button("Complete")  # ✅ Fixed spelling
+list_box = fsg.Listbox(values=Functions.get_todos(), key='todos',
+                       enable_events=True,
+                       size=[45, 10])
+edit_button = fsg.Button("Edit")
+exit_button = fsg.Button("Exit")
+
 window = fsg.Window("Todo List App",
-                    layout =[[lable],[input_box,add_button]],
-                    font=("Helvetica",20))
+                    layout=[[label],
+                            [input_box, add_button],
+                            [list_box, edit_button, complete_button],
+                            [exit_button]],
+                    font=("Helvetica", 20))
+
 while True:
-    event, values =window.read()
+    event, values = window.read()
+    print(event)
+    print(values)
+
     match event:
         case "Add":
-            todos =Functions.get_todos()
-            new_todo = values['todo'] +"\n"
+            todos = Functions.get_todos()
+            new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             Functions.write_todos(todos)
-        case fsg.WINDOW_CLOSED:
+            window['todos'].update(values=todos)
+
+        case "Edit":
+            try:
+                todo_to_edit = values['todos'][0]
+                new_todo = values['todo']
+                todos = Functions.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo + "\n"
+                Functions.write_todos(todos)
+                window['todos'].update(values=todos)
+            except IndexError:
+                fsg.popup("Please select a todo to edit.")
+
+        case "Complete":
+            try:
+                todo_to_complete = values['todos'][0]
+                todos = Functions.get_todos()
+                todos.remove(todo_to_complete)
+                Functions.write_todos(todos)
+                window['todos'].update(values=todos)
+                window['todo'].update(value="")
+            except IndexError:
+                fsg.popup("Please select a todo to complete.")
+
+        case "Exit":
             break
 
-window.close()
+        case "todos":
+            window['todo'].update(value=values['todos'][0])
 
-
-
-
-
-
-
-
+        case fsg.WINDOW_CLOSED:
+            break
 
 window.close()
